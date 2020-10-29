2b47690d63a37317ca5f9e90dafae0a415f5212e
Michell Cronberg <michell@cronberg.dk>
2020-10-29 14:10:34 +0100

    Add project files.

---
+++ Days2Xmas_ASPNETMVC_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Days2Xmas_ASPNETMVC_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
